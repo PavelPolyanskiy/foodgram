3da26c017d5eb3d02161677a326fa1116c45fc2f
Pavel Polyanskiy <pavel926724@yandex.ru>
2025-08-21 22:44:51 +0300

    import_recipes feature

--- backend/api/management/commands/import_recipes.py
+++ backend/api/management/commands/import_recipes.py
@@ -1,46 +1,100 @@
-# import json
-# import os
+import json
+import random
 
-# from django.conf import settings
-# from django.core.management.base import BaseCommand
-# from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.db import transaction
 
-# from recipe.models import Recipe
-
-# User = get_user_model()
+from recipe.models import Ingredient, Recipe, IngredientRecipe, Tag
 
 
-# class Command(BaseCommand):
-#     help = 'Импорт данных из CSV файлов'
+User = get_user_model()
 
-#     def handle(self, *args, **kwargs):
-#         data_dir = os.path.join(settings.BASE_DIR, 'data')
 
-#         file_data = self.__get_data_from_file(data_dir, 'recipes.json')
-#         self._create_user
-#         author = User.objects.filter(username='autochef')
-#         for el in file_data:
-#             Recipe.objects.update_or_create(
-#                 author=author,
-#                 name=el['name'],
-#                 image=el['image'],
-#                 ingredients=el['ingredients'],
-#                 tags=el['tags'],
-#                 cooking_time=el['cooking_time'],
-#             )
+class Command(BaseCommand):
+    fake_image = ''
+    author = None
+    users = User.objects.all()
 
-#     def _create_user(self):
-#         User.objects.create(
-#             email='chef@foodgram.ru',
-#             username='autochef',
-#             first_name='Повар',
-#             last_name='Шеф'
-#         )
+    def get_author(self):
+        return random.choice(self.users)
 
-#     def __get_data_from_file(self, data_dir, file_name):
-#         with open(
-#             file=os.path.join(data_dir, file_name),
-#             encoding='utf-8'
-#         ) as file:
-#             data = json.load(file)
-#         return data
+    def handle(self, *args, **options):
+        file_path = settings.BASE_DIR / 'data/recipes.json'
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        try:
+            for recipe_data in data:
+                self._create_recipe(recipe_data)
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(str(e)))
+
+    def _create_user(self):
+        User.objects.create(
+            email='chef@foodgram.ru',
+            username='autochef',
+            first_name='Повар',
+            last_name='Шеф'
+        )
+
+    def _create_recipe(self, recipe_data):
+        if not self.users.exists():
+            self._create_user()
+            self.users = User.objects.all()
+
+        tags = recipe_data.pop('tags')
+        ingredients = recipe_data.pop('ingredients')
+        recipe_name = recipe_data['name']
+
+        if Recipe.objects.filter(name=recipe_name).exists():
+            self.stdout.write(
+                self.style.WARNING(
+                    f'Рецепт {recipe_name!r} уже существует! Пропускаем.'
+                )
+            )
+            return
+
+        try:
+            with transaction.atomic():
+                recipe, created = Recipe.objects.update_or_create(
+                    author=self.get_author(),
+                    defaults=recipe_data
+                )
+
+                tag_objects = Tag.objects.filter(id__in=tags)
+                recipe.tags.set(tag_objects)
+
+                for ingredient in ingredients:
+
+                    ingredient_id = ingredient['id']
+                    if not Ingredient.objects.filter(
+                        id=ingredient_id
+                    ).exists():
+                        raise ValueError(
+                            f'Ингредиент с ID {ingredient_id} не существует!'
+                        )
+
+                    current_ingredient = Ingredient.objects.get(
+                        id=ingredient_id
+                    )
+                    IngredientRecipe.objects.create(
+                        recipe=recipe,
+                        ingredient=current_ingredient,
+                        amount=ingredient['amount'],
+                    )
+
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Успешно создан рецепт {recipe_name!r}!'
+                    )
+                )
+
+        except Exception as e:
+            self.stdout.write(
+                self.style.ERROR(
+                    f'Ошибка при создании рецепта {recipe_name}: {str(e)}'
+                )
+            )
